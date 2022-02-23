26fc661ed41e0afbfa02794dfc6d9b36e7814ff8
KohakuCao <kohaku@hepnovel.com>
2022-02-23 14:31:25 +0800

    fix login page footer

--- login.php
+++ login.php
@@ -35,8 +35,9 @@ if(isset($_GET["for"])){
 		</div>
 	</main>
 	<footer style="bottom: 0; width: 100%">
-	<div class="container-fluid bg-dark text-white" style="height: 96px;z-index:-999">
-		<h5 class="px-auto text-center">Reimu</h5>
+	<div class="container-fluid text-white page-footer" style="height: 128px;z-index:-999">
+		<h5 class="px-auto text-center">灵Reimu 综合用户中心</h5>
+		<p class="text-center">由星云娘DevTeam开发<br />背景人物与Logo设计均源自东方Project，其版权归上海爱丽丝幻乐团与ZUN所有<br />背景图自<a href="https://www.pixiv.net/artworks/34844544">https://www.pixiv.net/artworks/34844544</a></p>
 	</div>
 	<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 style="display: none;" src="//music.163.com/outchain/player?type=2&id=41652392&auto=1&height=66"></iframe>
 	</footer>
